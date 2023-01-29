0278bab4c96a249c9b40878049f0d6ea61ef8fa3
Dolfour <rodolfohenriques23@gmail.com>
2023-01-29 18:03:32 -0300

    Realizando Entrega

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,20 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function showUpModal(){
+    const modalContainer = document.querySelector(".modalWrapper");
+    const buttons = document.querySelectorAll(".showModal");
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            modalContainer.showModal();
+    
+            closeModal();
+        });
+    }); 
+}
+
+function closeModal(){
+    const closeBtn = document.querySelector(".closeBtn");
+    const modalContainer = document.querySelector(".modalWrapper");
+    closeBtn.addEventListener("click", () => {
+        modalContainer.close();
+    })
+}
+showUpModal();
